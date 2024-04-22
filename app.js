bd6a38c70d8d6c3ca1ba32a7bda0d5b5b297250f
edo98811 <edoardofilippi77@gmail.com>
2024-04-22 15:51:35 +0200

    navbar error

--- app.js
+++ app.js
@@ -1,9 +1,4 @@
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-  }
+document.querySelector('.burger-menu').addEventListener('click', () => {
+  const navLinks = document.querySelector('.nav-links');
+  navLinks.classList.toggle('show');
+});
